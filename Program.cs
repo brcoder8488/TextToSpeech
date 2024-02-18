f325d63c5f5a81357c5200403e345298f2c25c50
Banti Rawat <130087901+brcoder8488@users.noreply.github.com>
2024-02-19 00:31:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace SampleSynthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output.
+            //synth.SetOutputToDefaultAudioDevice();
+
+            // Set the voice
+            //synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            synth.SelectVoiceByHints(VoiceGender.Neutral, VoiceAge.Child);
+            // Speak a string.
+            //synth.Speak("This example demonstrates a basic use of Speech Synthesizer");
+            synth.Speak("Hi! I'm Banti Rawat From Uttar Pradesh.");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
